51d7a35a2cdc03b2a92128e26d7a7085e3330a8b
Lee JongCheol <whdcjf817@naver.com>
2025-09-11 22:56:30 +0900

    배경, 그라데이션 등

---
+++ Section02/NumberGuessGame/screens/GameScreen.js
@@ -0,0 +1,9 @@
+function GameScreen() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
+
+export default GameScreen;
---
+++ Section02/NumberGuessGame/screens/StartGameScreen.js
@@ -0,0 +1,67 @@
+import { StyleSheet, TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+import { useState } from "react";
+
+function StartGameScreen() {
+  const [enteredNumber, setEnteredNumber] = useState("");
+
+  function numberInputHandler(enteredText) {
+    setEnteredNumber(enteredText);
+  }
+
+  function confirmInputHandler() {}
+
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+        onChangeText={numberInputHandler}
+        value={enteredNumber}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 15,
+    marginTop: 100,
+    marginHorizontal: 24,
+    backgroundColor: "#802b55ff",
+    borderRadius: 8,
+    elevation: 4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  numberInput: {
+    height: 55,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+});
+
+export default StartGameScreen;
